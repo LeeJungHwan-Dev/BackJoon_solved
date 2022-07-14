6e52be4a49801d8c5aa8ba1d0de98391ab6fe769
LeeJungHwan-Dev <aftermoa@gmail.com>
2022-07-14 13:17:36 +0900

    solved 9498번 해결
    이 문제는 기본적인 if else 문의 사용법을 알면 쉽게 해결 할 수 있다.

---
+++ src/main/kotlin/Conditional/examination_results_9498.kt
@@ -0,0 +1,19 @@
+package Conditional
+
+fun main() {
+    val number = readLine()!!.toInt()
+
+
+    if(number >= 90){
+        println("A")
+    }else if(number >= 80){
+        println("B")
+    }else if(number >= 70){
+        println("C")
+    }else if(number >= 60){
+        println("D")
+    }else{
+        println("F")
+    }
+
+}
