89e0d674db6c91a3b318cf9a639db1c9ea4b848e
LeeJungHwan-Dev <aftermoa@gmail.com>
2022-07-14 13:30:03 +0900

    solved 2753번 해결
    이 문제는 기본적인 if else 문의 사용법을 알면 쉽게 해결 할 수 있다. 추가적으로 and , or , not에 대한 개념도 알아두면 좋다.

---
+++ src/main/kotlin/Conditional/leap_year_2753.kt
@@ -0,0 +1,12 @@
+package Conditional
+
+fun main() {
+    val year = readLine()!!.toInt()
+
+    if(year % 4 == 0 && year % 100 != 0 || year % 4 == 0 && year % 400 == 0){
+        println(1)
+    }else{
+        println(0)
+    }
+
+}
